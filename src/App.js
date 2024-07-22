face857fa9adf18ece03bcfb70c872d489d758d5
palthepusaisahith <palthepusaisahith66@gmail.com>
2024-07-22 17:37:54 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -2,40 +2,51 @@ import React from 'react';
 import Navbar from './navbar/NavBar';
 import PathFinder from './pathfinder/PathFinder';
 import './App.css';
+
 /*
+    This comment block describes the schema for a 2D array.
+    Each element of the 2D array represents a node with row and column properties.
 
-1. Create a 2D array
-2. Each 2D array will be a node
-
-    /
-        SCHEMA OF THE 2D ARRAY
-        [
-            [{row , col} , {row , col} , {row , col}]
-            [{row , col} , {row , col} , {row , col}]
-            [{row , col} , {row , col} , {row , col}]
-        ]
-    /
-
+    SCHEMA OF THE 2D ARRAY
+    [
+        [{row, col}, {row, col}, {row, col}],
+        [{row, col}, {row, col}, {row, col}],
+        [{row, col}, {row, col}, {row, col}]
+    ]
 */
+
 const App = () => {
 
+    // Function to handle click events, reloading the page
     const clickhandler = () => {
         window.location.reload();
     }
 
     return (
         <div className='bodyWrapper'>
+            {/* Rendering the Navbar component */}
             <Navbar/>
-            {/* <h4 className='heading'> Visualize the algorithms </h4> */}
+            {/* 
+                <h4 className='heading'> Visualize the algorithms </h4> 
+                Commented out heading for future use if needed
+            */}
             <div className='util'>
-                <div> <input className='color' type="color" value={'#02030a'} disabled /> Wall</div>
-                {/* <div> <button onClick={clickhandler} className="utilButton"> clear Board </button> </div> */}
+                <div> 
+                    {/* Disabled color input for representing wall color */}
+                    <input className='color' type="color" value={'#02030a'} disabled /> Wall
+                </div>
+                {/* 
+                    <div> 
+                        <button onClick={clickhandler} className="utilButton"> clear Board </button> 
+                    </div>
+                    Commented out button for clearing the board, which uses clickhandler function
+                */}
             </div>
             <div className='main'>
+                {/* Rendering the PathFinder component */}
                 <PathFinder />
             </div>
         </div>
-
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'; // Import React library
+import ReactDOM from 'react-dom/client'; // Import ReactDOM for rendering components
+import App from './App'; // Import the main App component
 
+// Create a root element by getting the element with the ID 'root' from the HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside the root element
 root.render(
   <App />
 );
